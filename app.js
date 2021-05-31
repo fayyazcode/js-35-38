c9f4b629c88fdee904e12dd517a53ee97b4c7b09
Fayyaz Alam <fayazalam.98@gmail.com>
2021-05-31 08:17:20 -0700

    Q3 done

--- app.js
+++ app.js
@@ -15,3 +15,13 @@
 //     document.write("Hello "+fullName)
 //    }
 //    user()
+
+//Q3
+// var first=+prompt("First number")
+// var second=+prompt("Second number")
+// function add(a,b) {
+//     var plus=(a+b)
+//    document.write(plus)
+// }
+
+// add(first,second)
